27892d7201bf383b52d7ea649d1fe9e10919e96a
Gabriel Farias <gabriel_ff@outlook.com>
2022-08-30 09:23:48 -0300

    Responsividade para telas maiores

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from './styles'
+
+export function Container() {
+    return(
+        <div></div>
+    );
+}
--- src/components/SobreMimContent/index.tsx
+++ src/components/SobreMimContent/index.tsx
@@ -13,7 +13,7 @@ export function SobreMimContent() {
 
                 <p>Sempre fui muito apaixonado por tecnologia, jogo videogame desde criança e sonhava em ser desenvolvedor de jogos. Com o tempo me aproximei da computação e percebi que é onde eu me encaixo e vejo mais oportunidades em formar uma carreira na área, por isso me especializei em desenvolvimento de software e a cada dia que passa eu sinto vontade de aprender e me especializar mais.</p>
 
-                <p>Comecei estudando lógica com Python, depois segui para banco de dados SQL, front-end (HTML, CSS, SASS e JS), e finalmente cheguei no Java, que apesar de mal falada, foi a linguagem que eu mais gostei de trabalhar, atualmente estou estudando frameworks de front-end (React, Angular e VueJS).</p>
+                <p>Comecei estudando lógica com Python, depois segui para banco de dados SQL, front-end (HTML, CSS, SASS, JS e ReactJS com TS), e finalmente cheguei no Java, que apesar de mal falada, foi a linguagem que eu mais gostei de trabalhar, atualmente estou estudando frameworks de front-end (Angular e VueJS).</p>
 
                 <p>Meus trabalhos sempre ocorrem de forma pontual, organizada e com qualidade, priorizo a qualidade e nunca me canso de buscar coisas novas, possuo muita disposição para crescer e aprender. Espero encontrar uma oportunidade para mostrar o meu potencial.</p>       
             </Content>
